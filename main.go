8b3581131067e177b9b723143762513123dca047
Finnbyte <eelpy@protonmail.com>
2024-03-08 11:12:04 +0200

    Initialize godotenv and set config options

--- main.go
+++ main.go
@@ -8,9 +8,18 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalln("Could not load .env file")
+	}
+
+	config.SetOption("clientId", os.Getenv("CLIENT_ID"))
+	config.SetOption("clientSecret", os.Getenv("CLIENT_SECRET"))
+	config.SetOption("port", os.Getenv("PORT"))
+
     http.HandleFunc("/", handlers.HandleIndex)
 	http.HandleFunc("/api/login", handlers.HandleLogin)
 
