adbd9b1568793afb52ca3119c43c1a85cca52c47
Finnbyte <eelpy@protonmail.com>
2024-03-09 16:25:08 +0200

    Add Avatar as a field in DiscordIdentity

--- discordApi/types.go
+++ discordApi/types.go
@@ -4,6 +4,7 @@ type DiscordIdentity struct {
     UserId string `json:"id"`
     Username string `json:"username"`
     Discriminator string `json:"discriminator"`
+    Avatar string `json:"avatar"`
 }
 
 type SuccesfulAuthentication struct {
